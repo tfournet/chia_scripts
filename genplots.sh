557fea0338262608bb995048c79f80d235c7e5f9
Tim Fournet <tfournet@radersolutions.com>
2021-05-06 12:13:49 -0500

    Update genplots.sh

--- genplots.sh
+++ genplots.sh
@@ -11,4 +11,6 @@ memsize="20000"
 k_size=32
 threads="2"
 
-chia plots create -k $k_size -n $plotcount -b $memsize -f $farmer_pubkey -p $plot_pubkey -t $tempdir -d $plotdir -r $threads
+cmd="chia plots create -k $k_size -n $plotcount -b $memsize -f $farmer_pubkey -p $farmpool_pubkey -t $tempdir -d $plotdir -r $threads"
+$cmd 
+
