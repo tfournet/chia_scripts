887aab77eea6a23d1ac480b0c0dccafd5d9167e4
Tim Fournet <tfournet@radersolutions.com>
2021-05-11 14:41:49 -0500

    threads

--- single-loop-plot.sh
+++ single-loop-plot.sh
@@ -7,14 +7,14 @@ source ~/chia-blockchain/chia_scripts/global.sh
 tempdir="/plots/tmp"
 plotdir="/plots/final"
 memsize=16384
-
+threads=4
 
 iteration=0
 while [ -d $tempdir ]; do
     iteration=$(($iteration + 1))
     echo "Creating plot $iteration" | tee ~/chia-blockchain/chia_scripts/chia.log
      sleep 3
-    chia plots create -k $k_size -n 4 -b $memsize -f $farmer_pubkey -p $farmpool_pubkey -t $tempdir -d $plotdir -r $threads
+    chia plots create -k $k_size -n 2 -b $memsize -f $farmer_pubkey -p $farmpool_pubkey -t $tempdir -d $plotdir -r $threads
     sleep 10
 done
 
