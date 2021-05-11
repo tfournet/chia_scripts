d5d5a8991b6502bd73d8e174c02750bcb6eddcc1
Tim Fournet <tfournet@radersolutions.com>
2021-05-11 15:22:22 -0500

    add hostname subdir

--- single-loop-plot.sh
+++ single-loop-plot.sh
@@ -4,14 +4,16 @@
 source ~/chia-blockchain/chia_scripts/global.sh
 
 
-tempdir="/plots/tmp"
+tempdir="/plots/tmp/$(hostname)"
 plotdir="/plots/final"
 memsize=16384
 threads=4
 k_size=32
 
+mkdir -p $tempdir 
+
 iteration=0
-while [ -d $tempdir ]; do
+while [ -d $plotdir ]; do
     iteration=$(($iteration + 1))
     echo "Creating plot $iteration" | tee ~/chia-blockchain/chia_scripts/chia.log
      sleep 3
