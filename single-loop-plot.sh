607a4bf5c29708c28a4f52ab8c077e6252ee736c
Tim Fournet <tfournet@radersolutions.com>
2021-05-11 14:41:17 -0500

    fix math

--- single-loop-plot.sh
+++ single-loop-plot.sh
@@ -11,7 +11,7 @@ memsize=16384
 
 iteration=0
 while [ -d $tempdir ]; do
-    iteration++
+    iteration=$(($iteration + 1))
     echo "Creating plot $iteration" | tee ~/chia-blockchain/chia_scripts/chia.log
      sleep 3
     chia plots create -k $k_size -n 4 -b $memsize -f $farmer_pubkey -p $farmpool_pubkey -t $tempdir -d $plotdir -r $threads
