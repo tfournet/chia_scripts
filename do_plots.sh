26e5af3bd4520467bfa814654e87e267d4713098
Tim Fournet <tfournet@tfour.net>
2021-06-17 13:50:55 -0500

    fix

--- do_plots.sh
+++ do_plots.sh
@@ -4,4 +4,7 @@ myhost=$(hostname | cut -f1 -d\.)
 
 script="${myhost}_plot.sh"
 
-curl https://github.com/tfournet/chia_scripts/$script | sh 
+rm -fr chia_scripts 
+git clone https://github.com/tfournet/chia_scripts
+sh chia_scripts/$script 
+
